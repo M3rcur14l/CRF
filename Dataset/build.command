5607cdc614bf54e7136a149a91cb34cc3feeb1d1
Antonello <m3rcur14l@hotmail.com>
2015-08-17 21:04:03 +0200

    first commit

---
+++ Dataset/build.command
@@ -0,0 +1,2 @@
+cd /Users/Antonello/CRF/Dataset
+/Applications/Praat.app/Contents/MacOS/Praat tokenizer.praat
---
+++ src/com/m3rc/crf/Runner.java
@@ -0,0 +1,17 @@
+package com.m3rc.crf;
+
+import java.io.IOException;
+
+/**
+ * Created by Antonello on 17/08/15.
+ */
+public class Runner {
+
+    public static void main(String[] args) throws IOException {
+
+        Process p = new ProcessBuilder("Dataset/build.command").start();
+
+    }
+
+
+}
