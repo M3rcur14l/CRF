6be9af93db048c7ff5c33edcab2f163bd029ac66
Antonello <m3rcur14l@hotmail.com>
2015-08-17 21:57:25 +0200

    second commit

--- src/com/m3rc/crf/Runner.java
+++ src/com/m3rc/crf/Runner.java
@@ -10,8 +10,10 @@ public class Runner {
     public static void main(String[] args) throws IOException {
 
         new ProcessBuilder("/Applications/Praat.app/Contents/MacOS/Praat",
-                "Dataset/tokenizer.praat").start();
-
+                "Dataset/tokenizer.praat").inheritIO().start();
+        new ProcessBuilder("python", "Dataset/postagger/pSCRDRtagger/EnRDRPOSTagger.py",
+                "tag", "../Models/English.RDR",
+                "../Dicts/English.DICT", "../../tokens.txt").inheritIO().start();
 
     }
 
